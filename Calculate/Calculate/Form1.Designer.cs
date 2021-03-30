a3448f900887f68528aae478e18bd44d0815a5b1
Eugene <Zhekul.90@gmail.com>
2021-03-30 16:44:39 +0300

    add GUI

---
+++ Calculate/Calculate/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace Calculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Reset = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Backspace = new System.Windows.Forms.Button();
+            this.Remains = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Seven = new System.Windows.Forms.Button();
+            this.Eight = new System.Windows.Forms.Button();
+            this.Nine = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.Four = new System.Windows.Forms.Button();
+            this.Five = new System.Windows.Forms.Button();
+            this.Six = new System.Windows.Forms.Button();
+            this.Subtract = new System.Windows.Forms.Button();
+            this.One = new System.Windows.Forms.Button();
+            this.Two = new System.Windows.Forms.Button();
+            this.Three = new System.Windows.Forms.Button();
+            this.Addition = new System.Windows.Forms.Button();
+            this.ChangeSing = new System.Windows.Forms.Button();
+            this.Zero = new System.Windows.Forms.Button();
+            this.Comma = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.Tomato;
+            this.Reset.Location = new System.Drawing.Point(30, 139);
+            this.Reset.Margin = new System.Windows.Forms.Padding(5);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 75);
+            this.Reset.TabIndex = 0;
+            this.Reset.Text = "CE";
+            this.Reset.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(30, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 44);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Backspace
+            // 
+            this.Backspace.BackColor = System.Drawing.Color.Tomato;
+            this.Backspace.Font = new System.Drawing.Font("Microsoft Tai Le", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Backspace.Location = new System.Drawing.Point(115, 139);
+            this.Backspace.Margin = new System.Windows.Forms.Padding(5);
+            this.Backspace.Name = "Backspace";
+            this.Backspace.Size = new System.Drawing.Size(75, 75);
+            this.Backspace.TabIndex = 2;
+            this.Backspace.Text = "BackSpace";
+            this.Backspace.UseVisualStyleBackColor = false;
+            // 
+            // Remains
+            // 
+            this.Remains.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Remains.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Remains.Location = new System.Drawing.Point(200, 139);
+            this.Remains.Margin = new System.Windows.Forms.Padding(5);
+            this.Remains.Name = "Remains";
+            this.Remains.Size = new System.Drawing.Size(75, 75);
+            this.Remains.TabIndex = 3;
+            this.Remains.Text = "%";
+            this.Remains.UseVisualStyleBackColor = false;
+            // 
+            // Multiply
+            // 
+            this.Multiply.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Multiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Multiply.Location = new System.Drawing.Point(285, 139);
+            this.Multiply.Margin = new System.Windows.Forms.Padding(5);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(75, 75);
+            this.Multiply.TabIndex = 4;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = false;
+            // 
+            // Seven
+            // 
+            this.Seven.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Seven.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Seven.Location = new System.Drawing.Point(30, 224);
+            this.Seven.Margin = new System.Windows.Forms.Padding(5);
+            this.Seven.Name = "Seven";
+            this.Seven.Size = new System.Drawing.Size(75, 75);
+            this.Seven.TabIndex = 5;
+            this.Seven.Text = "7";
+            this.Seven.UseVisualStyleBackColor = false;
+            // 
+            // Eight
+            // 
+            this.Eight.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Eight.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Eight.Location = new System.Drawing.Point(115, 224);
+            this.Eight.Margin = new System.Windows.Forms.Padding(5);
+            this.Eight.Name = "Eight";
+            this.Eight.Size = new System.Drawing.Size(75, 75);
+            this.Eight.TabIndex = 6;
+            this.Eight.Text = "8";
+            this.Eight.UseVisualStyleBackColor = false;
+            // 
+            // Nine
+            // 
+            this.Nine.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Nine.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Nine.Location = new System.Drawing.Point(200, 224);
+            this.Nine.Margin = new System.Windows.Forms.Padding(5);
+            this.Nine.Name = "Nine";
+            this.Nine.Size = new System.Drawing.Size(75, 75);
+            this.Nine.TabIndex = 7;
+            this.Nine.Text = "9";
+            this.Nine.UseVisualStyleBackColor = false;
+            // 
+            // Divide
+            // 
+            this.Divide.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Divide.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Divide.Location = new System.Drawing.Point(285, 224);
+            this.Divide.Margin = new System.Windows.Forms.Padding(5);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(75, 75);
+            this.Divide.TabIndex = 8;
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = false;
+            // 
+            // Four
+            // 
+            this.Four.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Four.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Four.Location = new System.Drawing.Point(30, 309);
+            this.Four.Margin = new System.Windows.Forms.Padding(5);
+            this.Four.Name = "Four";
+            this.Four.Size = new System.Drawing.Size(75, 75);
+            this.Four.TabIndex = 9;
+            this.Four.Text = "4";
+            this.Four.UseVisualStyleBackColor = false;
+            // 
+            // Five
+            // 
+            this.Five.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Five.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Five.Location = new System.Drawing.Point(115, 309);
+            this.Five.Margin = new System.Windows.Forms.Padding(5);
+            this.Five.Name = "Five";
+            this.Five.Size = new System.Drawing.Size(75, 75);
+            this.Five.TabIndex = 10;
+            this.Five.Text = "5";
+            this.Five.UseVisualStyleBackColor = false;
+            // 
+            // Six
+            // 
+            this.Six.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Six.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Six.Location = new System.Drawing.Point(200, 309);
+            this.Six.Margin = new System.Windows.Forms.Padding(5);
+            this.Six.Name = "Six";
+            this.Six.Size = new System.Drawing.Size(75, 75);
+            this.Six.TabIndex = 11;
+            this.Six.Text = "6";
+            this.Six.UseVisualStyleBackColor = false;
+            // 
+            // Subtract
+            // 
+            this.Subtract.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Subtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Subtract.Location = new System.Drawing.Point(285, 309);
+            this.Subtract.Margin = new System.Windows.Forms.Padding(5);
+            this.Subtract.Name = "Subtract";
+            this.Subtract.Size = new System.Drawing.Size(75, 75);
+            this.Subtract.TabIndex = 12;
+            this.Subtract.Text = "-";
+            this.Subtract.UseVisualStyleBackColor = false;
+            // 
+            // One
+            // 
+            this.One.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.One.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.One.Location = new System.Drawing.Point(30, 394);
+            this.One.Margin = new System.Windows.Forms.Padding(5);
+            this.One.Name = "One";
+            this.One.Size = new System.Drawing.Size(75, 75);
+            this.One.TabIndex = 13;
+            this.One.Text = "1";
+            this.One.UseVisualStyleBackColor = false;
+            // 
+            // Two
+            // 
+            this.Two.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Two.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Two.Location = new System.Drawing.Point(115, 394);
+            this.Two.Margin = new System.Windows.Forms.Padding(5);
+            this.Two.Name = "Two";
+            this.Two.Size = new System.Drawing.Size(75, 75);
+            this.Two.TabIndex = 14;
+            this.Two.Text = "2";
+            this.Two.UseVisualStyleBackColor = false;
+            // 
+            // Three
+            // 
+            this.Three.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Three.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Three.Location = new System.Drawing.Point(200, 394);
+            this.Three.Margin = new System.Windows.Forms.Padding(5);
+            this.Three.Name = "Three";
+            this.Three.Size = new System.Drawing.Size(75, 75);
+            this.Three.TabIndex = 15;
+            this.Three.Text = "3";
+            this.Three.UseVisualStyleBackColor = false;
+            // 
+            // Addition
+            // 
+            this.Addition.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Addition.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Addition.Location = new System.Drawing.Point(285, 394);
+            this.Addition.Margin = new System.Windows.Forms.Padding(5);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(75, 75);
+            this.Addition.TabIndex = 16;
+            this.Addition.Text = "+";
+            this.Addition.UseVisualStyleBackColor = false;
+            // 
+            // ChangeSing
+            // 
+            this.ChangeSing.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ChangeSing.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChangeSing.Location = new System.Drawing.Point(30, 479);
+            this.ChangeSing.Margin = new System.Windows.Forms.Padding(5);
+            this.ChangeSing.Name = "ChangeSing";
+            this.ChangeSing.Size = new System.Drawing.Size(75, 75);
+            this.ChangeSing.TabIndex = 17;
+            this.ChangeSing.Text = "±";
+            this.ChangeSing.UseVisualStyleBackColor = false;
+            // 
+            // Zero
+            // 
+            this.Zero.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Zero.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Zero.Location = new System.Drawing.Point(115, 479);
+            this.Zero.Margin = new System.Windows.Forms.Padding(5);
+            this.Zero.Name = "Zero";
+            this.Zero.Size = new System.Drawing.Size(75, 75);
+            this.Zero.TabIndex = 18;
+            this.Zero.Text = "0";
+            this.Zero.UseVisualStyleBackColor = false;
+            // 
+            // Comma
+            // 
+            this.Comma.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Comma.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Comma.Location = new System.Drawing.Point(200, 479);
+            this.Comma.Margin = new System.Windows.Forms.Padding(5);
+            this.Comma.Name = "Comma";
+            this.Comma.Size = new System.Drawing.Size(75, 75);
+            this.Comma.TabIndex = 19;
+            this.Comma.Text = ",";
+            this.Comma.UseVisualStyleBackColor = false;
+            // 
+            // Result
+            // 
+            this.Result.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result.Location = new System.Drawing.Point(285, 479);
+            this.Result.Margin = new System.Windows.Forms.Padding(5);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(75, 75);
+            this.Result.TabIndex = 20;
+            this.Result.Text = "=";
+            this.Result.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 589);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Comma);
+            this.Controls.Add(this.Zero);
+            this.Controls.Add(this.ChangeSing);
+            this.Controls.Add(this.Addition);
+            this.Controls.Add(this.Three);
+            this.Controls.Add(this.Two);
+            this.Controls.Add(this.One);
+            this.Controls.Add(this.Subtract);
+            this.Controls.Add(this.Six);
+            this.Controls.Add(this.Five);
+            this.Controls.Add(this.Four);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Nine);
+            this.Controls.Add(this.Eight);
+            this.Controls.Add(this.Seven);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Remains);
+            this.Controls.Add(this.Backspace);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Reset);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Backspace;
+        private System.Windows.Forms.Button Remains;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Seven;
+        private System.Windows.Forms.Button Eight;
+        private System.Windows.Forms.Button Nine;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Button Four;
+        private System.Windows.Forms.Button Five;
+        private System.Windows.Forms.Button Six;
+        private System.Windows.Forms.Button Subtract;
+        private System.Windows.Forms.Button One;
+        private System.Windows.Forms.Button Two;
+        private System.Windows.Forms.Button Three;
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.Button ChangeSing;
+        private System.Windows.Forms.Button Zero;
+        private System.Windows.Forms.Button Comma;
+        private System.Windows.Forms.Button Result;
+    }
+}
+
---
+++ Calculate/Calculate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Calculate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculate")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("18eb5894-d176-4af9-af3f-0a022d2935fc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
